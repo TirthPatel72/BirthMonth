8fc47d9d11fe805c9b66dd80d8db8aedd9b56b9a
TirthPatel72 <104174533+TirthPatel72@users.noreply.github.com>
2025-09-29 12:14:21 -0400

    Initial Commit

--- src/BirthMonth.java
+++ src/BirthMonth.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class BirthMonth {
+public class BirthMonth
+{
+    public static void main(String[] args)
+    {
+        int birthMonth = 0;
+        String trash;
+
+       Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter your birth month (1-12): ");
+        if (in.hasNextInt())
+        {
+
+            birthMonth = in.nextInt();
+            in.nextLine(); // Consume newline
+
+            if (birthMonth >= 1 && birthMonth <= 12) {
+                System.out.println("You were born in month: " + birthMonth);
+            }
+            else
+            {
+                System.out.println("Invalid month number: " + birthMonth);
+                System.out.println("Exiting... Run the program again");
+                System.exit(0);
+            }
+        }
+            else
+            {
+                trash = in.nextLine(); // Consume invalid input
+
+                System.out.println("Invalid input. Please enter a number between 1 and 12.");
+                System.out.println("Exiting... Run the program again");
+                System.exit(0);
+            }
+
+    }
+
 }
+
+
+
